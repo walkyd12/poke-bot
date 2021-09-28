872e94988687d8e6c2859cea3c21a0d58665a952
walkyd12 <schmidt.walker@yahoo.com>
2021-09-28 13:22:33 -0400

    added functions for checking the screen

--- web/server.py
+++ web/server.py
@@ -11,58 +11,68 @@ import PokeVision as pv
 
 app = Flask(__name__)
 
-def is_running():
-	stream = os.popen('./is_running.sh')
-	out = stream.read()
-	print(out)
-	if out is not None and out != "":
-		return ["Running...", out.split(' ')[3]]
-	return ["Not running!", "00:00:00"]
+class PokeWeb():
+	def is_running():
+		stream = os.popen('./is_running.sh')
+		out = stream.read()
+		print(out)
+		if out is not None and out != "":
+			return ["Running...", out.split(' ')[3]]
+		return ["Not running!", "00:00:00"]
 
-@app.route('/')
-def index():
-	csv_filename = 'dominant_color.csv'
-	plot_filename = 'viz.png'
-	df = pd.read_csv(f'{app.static_folder}/{csv_filename}')
-	num_checks = int(df.count()[0])
+	@app.route('/')
+	def index():
+		csv_filename = 'dominant_color.csv'
+		plot_filename = 'viz.png'
+		df = pd.read_csv(f'{app.static_folder}/{csv_filename}')
+		num_checks = int(df.count()[0])
 
-	output_obj = calculate_chance(1, 4096, num_checks)
-	is_run = is_running()
-	output_obj['is_running'] = is_run[0]
-	output_obj['cur_runtime'] = is_run[1]
-	output_obj['is_running_col'] = "lightgreen" if "Running" in is_run[0] else "crimson"
-	viz(app.static_folder, csv_filename, plot_filename)
+		output_obj = calculate_chance(1, 4096, num_checks)
+		is_run = is_running()
+		output_obj['is_running'] = is_run[0]
+		output_obj['cur_runtime'] = is_run[1]
+		output_obj['is_running_col'] = "lightgreen" if "Running" in is_run[0] else "crimson"
+		viz(app.static_folder, csv_filename, plot_filename)
 
-	return render_template('index.html', value=output_obj)
+		return render_template('index.html', value=output_obj)
 
-@app.route("/upload-complete", methods=["POST"])
-def handle_upload_response():
-	"""This will be called after every upload, but we can ignore it"""
-	return "Success"
+	@app.route("/upload-complete", methods=["POST"])
+	def handle_upload_response():
+		"""This will be called after every upload, but we can ignore it"""
+		return "Success"
 
-@app.route("/upload", methods=["POST"])
-def upload():
-	io_file = request.files["media"]
-	contents = io_file.read()
-	if contents == b'':
-		return {'statusCode':405}
-	jpg_as_np = np.frombuffer(contents, dtype=np.uint8)
-	img = cv2.imdecode(jpg_as_np, flags=1)
-	cv2.imwrite("upload.png", img)
-	
-	cm = pv.PokeVision(base_path='/home/waklky/poke-bot',asset_folder='ultrasun_assets')
+	@app.route("/upload", methods=["POST"])
+	def upload():
+		io_file = request.files["media"]
+		contents = io_file.read()
+		if contents == b'':
+			return {'statusCode':405}
+		jpg_as_np = np.frombuffer(contents, dtype=np.uint8)
+		img = cv2.imdecode(jpg_as_np, flags=1)
+		cv2.imwrite("upload.png", img)
 
-	print(cm.is_battle_screen("upload.png"))
-	
-	return {'statusCode':200}
+		#cm = pv.PokeVision(base_path='/home/waklky/poke-bot',asset_folder='ultrasun_assets')
 
-# No caching at all for API endpoints.
-@app.after_request
-def add_header(response):
-    # response.cache_control.no_store = True
-    if 'Cache-Control' not in response.headers:
-        response.headers['Cache-Control'] = 'no-store'
-    return response
+		return { 'statusCode':200 }
+
+	@app.route("/screen-check", methods=["GET"])
+	def screen_check():
+		cm = pv.PokeVision(base_path='/home/waklky/poke-bot',asset_folder='ultrasun_assets')
+
+		res = {}
+		res['battle_screen'] = {}
+		res['battle_screen']['is_battle'] = cm.is_battle_screen("upload.png")
+		res['battle_screen']['pname'] = cm.check_battle_mon("upload.png", ['drifloon', 'gastly'])
+
+		return { 'statusCode':200, 'response': res}
+
+	# No caching at all for API endpoints.
+	@app.after_request
+	def add_header(response):
+	    # response.cache_control.no_store = True
+	    if 'Cache-Control' not in response.headers:
+	        response.headers['Cache-Control'] = 'no-store'
+	    return response
 
 if __name__ == '__main__':
 	app.config.from_object('config')
